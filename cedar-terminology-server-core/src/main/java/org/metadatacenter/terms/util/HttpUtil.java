a51966650ad3557a8858b500f67d4da92c04a76c
Marcos Martinez <marcos.martinez.romero@gmail.com>
2016-05-02 18:11:31 -0700

    Disable BioPortal request retry

--- cedar-terminology-server-core/src/main/java/org/metadatacenter/terms/util/HttpUtil.java
+++ cedar-terminology-server-core/src/main/java/org/metadatacenter/terms/util/HttpUtil.java
@@ -11,21 +11,21 @@ public class HttpUtil {
 
   public static HttpResponse makeHttpRequest(Request request) throws IOException {
     HttpResponse response = request.execute().returnResponse();
-    int statusCode = response.getStatusLine().getStatusCode();
-    int count = 0;
-    int maxAttempts = 20;
+//    int statusCode = response.getStatusLine().getStatusCode();
+//    int count = 0;
+//    int maxAttempts = 20;
     // Repeat some requests for safety
-    while ((statusCode == 429 || statusCode == 500) && count++ < maxAttempts) {
-      System.out.println("BioPortal returned error " + statusCode + ". Trying it again...");
-      //Delay between calls
-      try {
-        Thread.sleep(BP_API_WAIT_TIME * 5);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      response = request.execute().returnResponse();
-      statusCode = response.getStatusLine().getStatusCode();
-    }
+//    while ((statusCode == 429 || statusCode == 500) && count++ < maxAttempts) {
+//      System.out.println("BioPortal returned error " + statusCode + ". Trying it again...");
+//      //Delay between calls
+//      try {
+//        Thread.sleep(BP_API_WAIT_TIME * 5);
+//      } catch (InterruptedException e) {
+//        e.printStackTrace();
+//      }
+//      response = request.execute().returnResponse();
+//      statusCode = response.getStatusLine().getStatusCode();
+//    }
     //Delay between calls to avoid status 429 (too many requests)
     try {
       Thread.sleep(BP_API_WAIT_TIME);
