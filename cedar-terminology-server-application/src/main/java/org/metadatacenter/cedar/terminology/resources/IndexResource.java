d0d39cb910451589f6ea97d93b96f1ba293a1095
Marcos Martinez <marcos.martinez.romero@gmail.com>
2017-02-27 14:14:33 -0800

    Use constructor from parent

--- cedar-terminology-server-application/src/main/java/org/metadatacenter/cedar/terminology/resources/IndexResource.java
+++ cedar-terminology-server-application/src/main/java/org/metadatacenter/cedar/terminology/resources/IndexResource.java
@@ -1,32 +1,12 @@
 package org.metadatacenter.cedar.terminology.resources;
 
-import com.codahale.metrics.annotation.Timed;
+import org.metadatacenter.cedar.util.dw.CedarMicroserviceIndexResource;
+import org.metadatacenter.config.CedarConfig;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import java.util.IdentityHashMap;
-import java.util.Map;
+public class IndexResource extends CedarMicroserviceIndexResource {
 
-@Path("/")
-@Produces(MediaType.APPLICATION_JSON)
-public class IndexResource {
-
-  private final static Map<String, Object> info;
-
-  static {
-    info = new IdentityHashMap<>();
-    info.put("name", "CEDAR Terminology Server");
-    info.put("documentation", "https://terminology." + System.getenv("CEDAR_HOST") + "/api");
+  public IndexResource(CedarConfig cedarConfig) {
+    super(cedarConfig, "CEDAR Terminology Server");
   }
 
-  public IndexResource() {
-  }
-
-  @GET
-  @Timed
-  public Map<String, Object> showInfo() {
-    return info;
-  }
 }
