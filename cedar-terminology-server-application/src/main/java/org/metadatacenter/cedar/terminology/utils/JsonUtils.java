aade55e22601243f6a42a49c70005316bea1b321
martinjoconnor <martin.oconnor@stanford.edu>
2017-11-06 16:37:50 -0800

    Removed Checker Framework

--- cedar-terminology-server-application/src/main/java/org/metadatacenter/cedar/terminology/utils/JsonUtils.java
+++ cedar-terminology-server-application/src/main/java/org/metadatacenter/cedar/terminology/utils/JsonUtils.java
@@ -2,13 +2,11 @@ package org.metadatacenter.cedar.terminology.utils;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.checkerframework.checker.nullness.qual.NonNull;
-
 public class JsonUtils
 {
 
   // Remove a particular field from a JsonNode object
-  public @NonNull JsonNode removeField(@NonNull JsonNode node, @NonNull String fieldName) {
+  public JsonNode removeField(JsonNode node, String fieldName) {
     ObjectNode object = (ObjectNode) node;
     object.remove(fieldName);
     return object;
