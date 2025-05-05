3be991d95022c1687dacc78f6c965691fbed7862
Attila L. Egyedi <attila.egyedi@gmail.com>
2025-05-05 16:10:18 -0700

    publication from BioPortal is a list now

--- cedar-terminology-server-core/src/main/java/org/metadatacenter/terms/domainObjects/OntologyDetails.java
+++ cedar-terminology-server-core/src/main/java/org/metadatacenter/terms/domainObjects/OntologyDetails.java
@@ -16,7 +16,7 @@ public class OntologyDetails implements Serializable {
   private String released;
   private String creationDate;
   private String homepage;
-  private String publication;
+  private List<String> publication;
   private String documentation;
   private String version;
   private boolean metricsAvailable;
@@ -26,7 +26,7 @@ public class OntologyDetails implements Serializable {
   }
 
   public OntologyDetails(String description, int numberOfClasses, List<String> categories, String
-      hasOntologyLanguage, String released, String creationDate, String homepage, String publication, String
+      hasOntologyLanguage, String released, String creationDate, String homepage, List<String> publication, String
                              documentation, String version, boolean metricsAvailable, boolean hasSubmissions) {
     this.description = description;
     this.numberOfClasses = numberOfClasses;
@@ -98,11 +98,11 @@ public class OntologyDetails implements Serializable {
     this.homepage = homepage;
   }
 
-  public String getPublication() {
+  public List<String> getPublication() {
     return publication;
   }
 
-  public void setPublication(String publication) {
+  public void setPublication(List<String> publication) {
     this.publication = publication;
   }
 
